79f5f08f506469a4cbcaa66ac0e87b4e17fb84bd
swapnesh <swapnesh20032003@gmail.com>
2019-04-22 14:52:50 +0530

    Updates

---
+++ precache-manifest.322f6a12dd9adf3496815fbac6ae823a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a85144cb2ca19f6de3f",
+    "url": "/react-simple-search/static/js/runtime~main.ffa24699.js"
+  },
+  {
+    "revision": "2bc9ac10f8e98003af8f",
+    "url": "/react-simple-search/static/js/main.8a5b5211.chunk.js"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/react-simple-search/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "2bc9ac10f8e98003af8f",
+    "url": "/react-simple-search/static/css/main.c11c8f9c.chunk.css"
+  },
+  {
+    "revision": "fec7caf203e5270d26941b3f10a45e56",
+    "url": "/react-simple-search/index.html"
+  }
+];
